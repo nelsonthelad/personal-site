e07f8083521f330d8d8d8bb16d23c8b2e5ff0d1b
nelsonthelad <nelsongdaniels@gmail.com>
2025-02-08 13:50:21 -0500

    Added Custom Font

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({ title, paragraph }) => {
   return (
     <div className="bg-transparent">
-      <div className="mx-4 mb-4 opacity-50 rounded-lg border border-gray-700 p-4 hover:border-gray-500 transition duration-300">
+      <div className="mx-4 mb-4 rounded-lg border border-secondary p-4 hover:border-accent transition duration-300">
         <div className="container flex flex-col justify-left">
           {title && <h1 className="text-white text-2xl font-semibold mb-2 mb-6">{title}</h1>}
           <p className="text-white text-lg">{paragraph}</p>
